fbadfec2aa70f1e2eacecc85301ba8cbeed15024
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2023-01-06 16:20:24 +0200

    Legatura Backend - Frontend

--- Backend/src/main/java/com/example/backend/BackendApplication.java
+++ Backend/src/main/java/com/example/backend/BackendApplication.java
@@ -10,4 +10,22 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    // Allow CORS 
+    // Bean code from: https://github.com/getarrays/employeemanager/blob/master/src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
+    @Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
+
 }
