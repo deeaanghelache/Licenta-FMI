97da0bee85d4ade558017897765c7db63f7ccc53
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2023-01-15 20:40:38 +0200

    Frontend setup

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomepageComponent,
+    AdminPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
