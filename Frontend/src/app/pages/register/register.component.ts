1f4fc1f1f38d1b33764917faf2e36a8487b097b9
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2023-02-10 11:23:19 +0200

    Fixed email validators

--- Frontend/src/app/pages/register/register.component.ts
+++ Frontend/src/app/pages/register/register.component.ts
@@ -28,7 +28,7 @@ export class RegisterComponent implements OnInit {
     {      
         firstName : ['', Validators.required],
         lastName : ['', Validators.required],
-        email : ['', Validators.required, Validators.email],
+        email : ['', Validators.compose([Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])],
         username : ['', Validators.required],
         password : ['', Validators.required],
         photo : [''],
