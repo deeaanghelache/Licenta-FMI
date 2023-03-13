91170a0792d8d4c109811153a41aab1f55c17e8a
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2023-03-13 14:25:49 +0200

    Started cities page layout

--- Frontend/src/app/pages/cities/cities.component.ts
+++ Frontend/src/app/pages/cities/cities.component.ts
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cities.component.scss']
 })
 export class CitiesComponent implements OnInit {
+  public admin:boolean = false;
+  public logged:boolean = false;
+  // public cities = [];
+  public cities = [
+    { name: "Paris", image: "", currency: "euro", country: "France"},
+    { name: "Paris", image: "", currency: "euro", country: "France"},
+    { name: "Paris", image: "", currency: "euro", country: "France"},
+    { name: "Paris", image: "", currency: "euro", country: "France"},
+    { name: "Paris", image: "", currency: "euro", country: "France"},    
+    { name: "Paris", image: "", currency: "euro", country: "France"},
+    { name: "Paris", image: "", currency: "euro", country: "France"},
+    { name: "Paris", image: "", currency: "euro", country: "France"},
+    { name: "Paris", image: "", currency: "euro", country: "France"},
+    { name: "Paris", image: "", currency: "euro", country: "France"},
+    { name: "Paris", image: "", currency: "euro", country: "France"},    
+    { name: "Paris", image: "", currency: "euro", country: "France"}
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.checkIfLoggedIn();
+    this.checkIfAdmin();
+
+    // TODO: get all cities from database
+  }
+
+  checkIfLoggedIn(){
+    if ("loggedUserEmail" in sessionStorage){
+      this.logged = true;
+    }
+  }
+
+  checkIfAdmin(){
+    if (sessionStorage.getItem("admin") === "admin"){
+      this.admin = true;
+    }
+  }
+
+  logout(){
+    sessionStorage.clear();
+    this.admin = false;
+    this.logged = false;
   }
 
 }
