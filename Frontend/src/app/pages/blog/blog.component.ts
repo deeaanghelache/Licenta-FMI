3064fa879410583e28915afd40e9af338c425661
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2023-05-03 14:15:50 +0300

    Blog homepage menu

--- Frontend/src/app/pages/blog/blog.component.ts
+++ Frontend/src/app/pages/blog/blog.component.ts
@@ -6,11 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
-
+  public logged:boolean = false;
+  public admin:boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.checkIfLoggedIn();
+    this.checkIfAdmin();
   }
 
+  checkIfLoggedIn(){
+    if ("loggedUserEmail" in sessionStorage){
+      this.logged = true;
+    }
+  }
+
+  checkIfAdmin(){
+    if (sessionStorage.getItem("admin") === "admin"){
+      this.admin = true;
+    }
+  }
+
+  logout(){
+    sessionStorage.clear();
+    this.admin = false;
+    this.logged = false;
+  }
 }
