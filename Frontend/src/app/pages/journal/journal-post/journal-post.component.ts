92502cfb3c14f55c043f313ac6d7987977ae722a
deeaanghelache <andreea.anghelache@s.unibuc.ro>
2023-05-08 14:47:58 +0300

    Generate journal page pdf done

--- Frontend/src/app/pages/journal/journal-post/journal-post.component.ts
+++ Frontend/src/app/pages/journal/journal-post/journal-post.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-journal-post',
@@ -13,4 +17,35 @@ export class JournalPostComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  generateJournalPostPdf() {
+    var img = new Image();
+    img.src = this.post['photo'];
+    img.onload = () => {
+      var canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      var canvasContext = canvas.getContext('2d');
+      if (canvasContext){
+        canvasContext.drawImage(img, 0, 0, img.width, img.height);
+        var imageUrl = canvas.toDataURL('image/png');
+        var docDefinition = {
+          content: [
+            { text: this.post['dateWritten'], fontSize: 12, italics: true, color: "gray" },
+            { text: " " },
+            { text: this.post['name'], fontSize: 15, bold: true },
+            { text: " " },
+            {
+              image: imageUrl,
+              width: 150
+            },
+            { text: " " },
+            { text: this.post['post'], fontSize: 12 },
+          ]
+        };
+        var pdfName = "journalPost_" + this.post['dateWritten'] + ".pdf";
+        pdfMake.createPdf(docDefinition).download(pdfName);
+      }
+    };
+  }
+  
 }
